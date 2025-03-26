1b88e2a47056fdc2dc6774fd78f89cbf1bc3c1bf
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-03-26 18:34:08 +0300

    feat: Added webauthn signup finish

--- qrigami/web_authn/views.py
+++ qrigami/web_authn/views.py
@@ -17,11 +17,15 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 
+from json import loads
+
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.http import HttpRequest, HttpResponseBadRequest, JsonResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.views.generic import FormView, View
-from webauthn import generate_registration_options
+from webauthn import generate_registration_options, verify_registration_response
+from webauthn.helpers.base64url_to_bytes import base64url_to_bytes
+from webauthn.helpers.bytes_to_base64url import bytes_to_base64url
 
 from web_authn.forms import SignUpForm
 from web_authn.serializers import WebAuthnJSONEncoder
@@ -51,4 +55,22 @@ class SignUpStartView(View):
             rp_name=settings.HOST_NAME,
             user_name=form.cleaned_data[User.username.field.name],
         )
+        request.session["challenge"] = bytes_to_base64url(options.challenge)
         return JsonResponse(options, WebAuthnJSONEncoder, safe=False)
+
+
+class SignUpFinishView(View):
+
+    """WebAuthn signup start view."""
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        """Process post requests."""
+        _verified_registration = verify_registration_response(
+            credential=loads(request.body.decode("utf-8")),
+            expected_challenge=base64url_to_bytes(request.session["challenge"]),
+            expected_rp_id=settings.HOST,
+            expected_origin=settings.ORIGIN,
+            require_user_presence=True,
+            require_user_verification=True,
+        )
+        return HttpResponse()
